95ade350a6aac67ede5788d88abd8c143156f501
gabqui <gabqui0405@skola.goteborg.se>
2022-02-08 14:09:51 +0100

    Added stuff in Person.java

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,8 +1,41 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+    man ska kunna:
+           ange namn
+           ha 100 health i början
+           increase health
+           decrease health
+ */
+
 public class Person {
+    private String name;
+    private int health;
+    //private List<String> inventory;
 
-    public static void main(String[] args) {
-        // TODO Auto-generated method stub
-
+    public Person(String name) {
+        this.name = name;
+        this.health = 100;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void increaseHealth(int currentHealth) {
+        if (currentHealth > 0) {
+            health += currentHealth;
+        }
+    }
+
+    public void decreaseHealth(int currentHealth) {
+        if (currentHealth < 0) {
+            health -= currentHealth;
+        }
+    }
+
+    public int getHealth() {
+        return health;
+    }
 }
