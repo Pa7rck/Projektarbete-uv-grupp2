65d19c89cfe6eb42d8b6568d91bbd6e450844fea
Patrick <patlin1003@skola.goteborg.se>
2022-02-09 20:25:24 +0100

    Add levels

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,42 +4,30 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
-
 	public static void main(String[] args) throws IOException {
 		Movement move = new Movement();
 
-		try(BufferedReader reader = new BufferedReader(new FileReader("Prologue.txt"));
-			Scanner scanner = new Scanner(System.in)) {
+		try (BufferedReader reader = new BufferedReader(new FileReader("Prologue.txt"));
+			 Scanner scanner = new Scanner(System.in)) {
 			String lines;
-			while((lines = reader.readLine()) != null) {
+			while ((lines = reader.readLine()) != null) {
 				System.out.println(lines);
 			}
 
-			System.out.println();
-			System.out.println("Which path do you choose?(left/right)");
-			String userChoice = scanner.next();
+			int level = 1;
+			while (level < 5) {
+				System.out.println();
+				System.out.println("Which path do you choose?(left/right)");
+				String userChoice = scanner.next();
 
-			if(userChoice.equals("left")) {
-				move.travelLeft();
-				if (move.getRandomEvent() >= 3) {
-					// event
-				} else {
-					// event
+				if (userChoice.equals("left")) {
+					move.travelLeft();
+				} else if (userChoice.equals("right")) {
+					move.travelRight();
 				}
-			} else if (userChoice.equals("right")) {
-				move.travelRight();
+
+				level++;
 			}
 		}
-
-
-
-
-
-
-
-		move.travelLeft();
-		System.out.println(move.getRandomEvent());
-
 	}
-
 }
