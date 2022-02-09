3fa40d4a64b2c11d89e0032f26b4b0682f141bdc
Adde0k <andkar0729@skola.goteborg.se>
2022-02-09 10:45:20 +0100

    Added two new events; findMushroom and stepOnNail.

--- src/main/java/Events.java
+++ src/main/java/Events.java
@@ -3,26 +3,26 @@ import java.util.Scanner;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class Events {
-    public static void BearAttack() {
+    public static void bearAttack() {
         int random;
         int choice;
 
         try (Scanner scanner = new Scanner(System.in)) {
             System.out.println("A bear appears from behind a tree, making its way towards you. What do you do?\n");
-            System.out.println("1. Approach the bear.\n2. Try to hide.");
+            System.out.println("1. Try to scare the bear.\n2. Try to hide.");
             choice = scanner.nextInt();
             if (choice == 1) {
                 random = ThreadLocalRandom.current().nextInt(1, 4);
                 if (random == 1){
                     System.out.println("The bear gets scared and runs away. You get away safely.");
                 } else {
-                    System.out.println("The bear decides to attack you, and you lose 30 HP.");
+                    System.out.println("The bear doesn't fear you at all.  It charges you, gets a swing off and runt away.\nYou lose 30 HP.");
                     Player.setHP(Player.getHP() - 30);
                 }
             } else if (choice == 2) {
                 random = ThreadLocalRandom.current().nextInt(1, 4);
                 if (random == 1){
-                    System.out.println("You try to hide behind a closeby rock, but the bear sees you. It charges you, gets a swing off and runt away. You lose 30 HP.");
+                    System.out.println("You try to hide behind a closeby rock, but the bear sees you. It charges you, gets a swing off and runt away.\nYou lose 30 HP.");
                     Player.setHP(Player.getHP() - 30);
                 } else {
                     System.out.println("You manage to hide behind a rock, and the bear doesn't notice you. It walks away and you're once again safe.");
@@ -33,5 +33,15 @@ public class Events {
         } catch (IOException e) {
             System.err.println("Error: " + e.getMessage());
         }
+    }
+
+    public static void findMushroom() {
+        System.out.println("You find a healthy-looking mushroom. You add it to your backpack.");
+        Player.addMushroom();
+    }
+
+    public static void stepOnNail() {
+        System.out.println("As you walk along the path, you step on an old, rusty nail. You take 5 HP.");
+        Player.setHP(Player.getHP() - 5);
     }
 }
