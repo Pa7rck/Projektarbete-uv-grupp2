50a323f27f56d4d38eb707e534e7e27830650b10
Patrick <patlin1003@skola.goteborg.se>
2022-02-09 20:46:42 +0100

    Add comments

--- src/main/java/Movement.java
+++ src/main/java/Movement.java
@@ -2,17 +2,30 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class Movement {
 
-    private int randomEvent;
+    // private int randomEvent;
+    // private int randomEvent2;
+
+    // Create an event object
 
     public void travelLeft() {
-        this.randomEvent = ThreadLocalRandom.current().nextInt(1, 50 + 1);
+        int randomEvent = ThreadLocalRandom.current().nextInt(1, 100 + 1);
+        if (randomEvent >= 33) {
+            // event
+            // consequences for player
+        } else {
+            // event
+            // consequences for player
+        }
     }
 
     public void travelRight() {
-        this.randomEvent = ThreadLocalRandom.current().nextInt(1, 100 + 1);
-    }
-
-    public int getRandomEvent() {
-        return randomEvent;
+        int randomEvent2 = ThreadLocalRandom.current().nextInt(1, 100 + 1);
+        if(randomEvent2 >= 33) {
+            // event
+            // consequences for player
+        } else {
+            // event
+            // consequences for player
+        }
     }
 }
