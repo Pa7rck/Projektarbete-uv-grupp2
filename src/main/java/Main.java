32297e781686ba2fa7c1f84a8bb856eb8f306deb
Patrick <patlin1003@skola.goteborg.se>
2022-02-09 19:38:41 +0100

    Add BufferedReader and initial userchoice

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,44 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws IOException {
+		Movement move = new Movement();
+
+		try(BufferedReader reader = new BufferedReader(new FileReader("Prologue.txt"));
+			Scanner scanner = new Scanner(System.in)) {
+			String lines;
+			while((lines = reader.readLine()) != null) {
+				System.out.println(lines);
+			}
+
+			System.out.println();
+			System.out.println("Which path do you choose?(left/right)");
+			String userChoice = scanner.next();
+
+			if(userChoice.equals("left")) {
+				move.travelLeft();
+				if (move.getRandomEvent() >= 3) {
+					// event
+				} else {
+					// event
+				}
+			} else if (userChoice.equals("right")) {
+				move.travelRight();
+			}
+		}
+
+
+
+
+
+
+
+		move.travelLeft();
+		System.out.println(move.getRandomEvent());
 
 	}
 
