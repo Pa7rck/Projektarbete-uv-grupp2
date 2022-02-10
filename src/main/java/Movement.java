996b34da0bcbc4de9189e09b4b24c8dd17017ea4
Patrick <patlin1003@skola.goteborg.se>
2022-02-10 13:01:10 +0100

    Add level counter

--- src/main/java/Movement.java
+++ src/main/java/Movement.java
@@ -2,30 +2,37 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class Movement {
 
-    // private int randomEvent;
-    // private int randomEvent2;
+    private int level = 1;
 
-    // Create an event object
+    // Create an event object here
 
     public void travelLeft() {
         int randomEvent = ThreadLocalRandom.current().nextInt(1, 100 + 1);
         if (randomEvent >= 33) {
-            // event
+            // call for event
             // consequences for player
         } else {
-            // event
+            // call for event
             // consequences for player
         }
+
+        this.level++;
     }
 
     public void travelRight() {
         int randomEvent2 = ThreadLocalRandom.current().nextInt(1, 100 + 1);
         if(randomEvent2 >= 33) {
-            // event
+            // call for event
             // consequences for player
         } else {
-            // event
+            // call for event
             // consequences for player
         }
+
+        this.level++;
+    }
+
+    public int getLevel() {
+        return level;
     }
 }
