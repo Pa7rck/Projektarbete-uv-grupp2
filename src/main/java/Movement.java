decd40655effba20a8a48b5e5776e955369f0d68
Patrick <patlin1003@skola.goteborg.se>
2022-02-12 12:16:29 +0100

    Add second argument to method

--- src/main/java/Movement.java
+++ src/main/java/Movement.java
@@ -3,30 +3,42 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class Movement {
 
+    private  int randomEvent;
+    private  int randomEvent2;
     private int level = 1;
 
     public void travelLeft(Scanner scanner) {
-        int randomEvent = ThreadLocalRandom.current().nextInt(1, 100 + 1);
+        this.randomEvent = ThreadLocalRandom.current().nextInt(1, 100 + 1);
         if (randomEvent <= 10) {
-            Events.eventBearAttack(scanner);
-        } else if (randomEvent <= 30 && randomEvent > 10 ){
+            Events.eventStepOnNail();
+        } else if (randomEvent <= 30 && randomEvent > 10 ) {
+           // Events.eventBearAttack(scanner, move);
+        } else if (randomEvent <= 50 && randomEvent > 30 ) {
             Events.eventPitTrap(scanner);
-        } else if (randomEvent <= 60 && randomEvent > 30 ){
+        } else if (randomEvent <= 100 && randomEvent > 50 ) {
+            Events.eventDeer();
+        }
+
+        this.level++;
+    }
+
+    public void travelRight(Scanner scanner, Movement move) {
+        randomEvent2 = ThreadLocalRandom.current().nextInt(1, 100 + 1);
+        if (randomEvent2 <= 100) {
+            Events.eventBearAttack(scanner, move);
+        } else {
 
         }
 
         this.level++;
     }
 
-    public void travelRight(Scanner scanner) {
-        int randomEvent2 = ThreadLocalRandom.current().nextInt(1, 100 + 1);
-        if (randomEvent2 <= 100) {
-            Events.eventBearAttack(scanner);
-        } else {
+    public int getRandomEvent() {
+        return randomEvent;
+    }
 
-        }
-
-        this.level++;
+    public int getRandomEvent2() {
+        return randomEvent2;
     }
 
     public int getLevel() {
