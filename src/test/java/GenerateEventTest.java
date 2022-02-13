f98757f841379b37980243d7ebeb1cc77d3da845
Patrick <patlin1003@skola.goteborg.se>
2022-02-13 14:40:56 +0100

    Change names

--- src/test/java/GenerateEventTest.java
+++ src/test/java/GenerateEventTest.java
@@ -2,26 +2,26 @@ import org.junit.jupiter.api.Test;
 import java.util.Scanner;
 import static org.junit.jupiter.api.Assertions.*;
 
-public class MovementTest {
+public class GenerateEventTest {
     Scanner sc1 = new Scanner("1");
     Scanner sc2 = new Scanner("2");
-    Movement move = new Movement();
+    GenerateEvent ge = new GenerateEvent();
 
     @Test
     public void testTurnLeft_Survive() {
 
-        move.travelLeft(sc1, move);
+        ge.generateEventLeft(sc1, ge);
         int expected = 2;
-        int actual = move.getLevel();
+        int actual = ge.getLevel();
         assertEquals(expected, actual);
     }
 
     @Test
     public void testTurnRight_Survive() {
 
-        move.travelRight(sc2, move);
+        ge.generateEventRight(sc2, ge);
         int expected = 2;
-        int actual = move.getLevel();
+        int actual = ge.getLevel();
         assertEquals(expected, actual);
     }
 }
